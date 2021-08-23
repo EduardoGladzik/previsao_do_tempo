03fa409c986fe7cffba870cc8baa29aded23c983
Eduardo_Gladzik_Rangel <eduardogladzik@gmail.com>
2021-08-23 15:04:34 -0300

    atualização do icone

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:previsao_do_tempo/screens/home/home.dart';
-import 'package:previsao_do_tempo/style.dart';
+import 'package:previsao_do_tempo/style.dart'; 
 
 void main() {
   runApp(
